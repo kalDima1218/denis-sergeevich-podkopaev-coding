e2a3b8fd543f17080f49ccb781b78f4fb73dc4d7
Dmitry Kalugin <dfkalugin@edu.hse.ru>
2021-11-16 19:56:20 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+def encodeInDSP(n):
+    res = ""
+    if n == 0: res = a1[0]
+    while n:
+        res+=a1[int(n%3)]
+        n//=3
+    return res
+
+
+def fillDictionary():
+    global a, a1, a2
+    a = " йцукенгшщзхъфывапролджэячсмитьбюё,.!"
+    a2 = {}
+    for i in range(0, 37):
+        a2[a[i]] = encodeInDSP(i)
+        print(a[i], a2[a[i]])
+
+
+def encode(s):
+    for i in s:
+        print(a2[i], end=" ")
+
+
+a1 = "д с п".split(" ")
+a1 = "Денис Сергеевич Подкопаев".split(" ")
+fillDictionary()
+
+### Непосредственно здесь происходит магия!
+s = "Здравствуйте Денис Сергеевич! Могу вас заверить, что новый год буду встречать в маске и перчатках.".lower()
+encode(s)
